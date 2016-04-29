b831b34de220decd56fb9b737a5f270c17acd4ff
wianvos <wvos@xebia.com>
2016-04-29 18:20:27 +0200

    added basic functionality

---
+++ src/main/resources/ted/PerformBuildNrCheck.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+import urllib
+import urllib2
+import json
+
+
+
+def getJsonDict(host, env):
+  """
+  Download the TED json for buildnumbers per environment
+  :param host: hostname of the ted server
+  :param env: wich environment the request the json for
+  :return: dict representation of the information provided by ted
+  """
+  url = "api/build-numbers/all/%s" % env
+  url = urllib.quote(url)
+  url = "http://%s/%s" % (host, url)
+
+  response = urllib2.urlopen(url)
+  return json.loads(response.read())
+
+def getModuleDict(host, env):
+  """
+  get the info from ted and clean it so we can use it
+  :param host: ted host
+  :param env: environment to query for
+  :return: a dictionary {moduleName: {info}}
+  """
+  cleanInfo = {}
+  rawInput = getJsonDict(host, env)
+
+  for x in rawInput:
+    for dep in x['departments']:
+        for domain in dep['domains']:
+          for module in domain['modules']:
+            if module.has_key('error') is False:
+                cleanModuleName =  module['moduleName'].split()[0]
+                cleanInfo[cleanModuleName] = {}
+
+                if module.has_key('buildNumbers'):
+                  cleanInfo[cleanModuleName] = module['buildNumbers']
+
+  return cleanInfo
+
+def getModuleInfo(host, env, module):
+  """
+    get specific info for one module
+    :param host: ted host
+    :param env: env to query for
+    :param module: module to get info for
+    :return: returns info for a module
+  """
+
+  moduleDict = getModuleDict(host, env)
+
+  if moduleDict.has_key(module) is False:
+        return False
+
+  return moduleDict[module]
+
+def checkModuleBuildNr(host, env, module, buildNumber):
+    """
+    check if the buildnr for the given module matches the buildnr from ted
+    :param host: ted host
+    :param env: env to query for
+    :param module: module name
+    :param buildNumber: buildnumber to match
+    :return: True of False
+    """
+    actualBuildNumber =  getModuleInfo(host, env, module)['buildNumber']
+    if actualBuildNumber != buildNumber:
+        print "buildNr: %s does not match up with result from TED: %s" % (buildNumber, actualBuildNumber)
+        return False
+
+    return True
+
+if checkModuleBuildNr(tedHost, environment, moduleName, buildNr):
+    print "Ted check succeeded"
+    return True
+
+sys.exit(2)
